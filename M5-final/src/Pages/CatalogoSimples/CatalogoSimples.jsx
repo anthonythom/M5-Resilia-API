5dcd1fb632d777cf2e209c4cd89d2541fa2d728b
Bruno <b.blanquez@yahoo.com.br>
2022-05-24 17:32:45 -0300

    iniciando busca de dados

--- M5-final/src/Pages/CatalogoSimples/CatalogoSimples.jsx
+++ M5-final/src/Pages/CatalogoSimples/CatalogoSimples.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import Header from '../../Components/Header/Header'
 import "./catalogo.css"
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 export default function CatalogoSimples() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://tattooresilia.herokuapp.com/catalogo`)
+      .then(resposta => {
+        setData(resposta.data.Tatuagens)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+  console.log(data)
+
   return (
-    <div>
-      {/* <Header /> */}
-      <h2>CATALOGOOO</h2>
+    <div className='divPrincipal'>
+      <div className="divSuperior">
+        <h1 className='titulo'>catalogo</h1>
+      </div>
+      <div>
+        <ul className='listaTattoo'>
+          {data.map((data, index) => (
+          <div key={index}>
+            {/* <Title text={data.name} /> */}
+            <li className='listaItens'>{data.DESCRICAO}</li> 
+            {console.log(data.DESCRICAO)}
+          </div>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }

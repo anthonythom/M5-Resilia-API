9f3534bb68261e63a15c13e5020e9bb4adf59fb2
Bruno <b.blanquez@yahoo.com.br>
2022-06-02 15:53:36 -0300

    adicionando estado de sucesso à criação de novo item

--- M5-final/src/Components/NovoItem/NovoItem.jsx
+++ M5-final/src/Components/NovoItem/NovoItem.jsx
@@ -9,55 +9,62 @@ export default function NovoItem() {
   const [tipo, setTipo] = useState("")
   const [rota, setRota] = useState("")
 
+  const estilo = {
+    resp: {
+      display: sucesso ? "none" : "flex",
+    },
+    resposta: {
+      display: sucesso ? "block" : "none"
+    }
+  }
+
   useEffect(() => {
      if (tipo == 1) {
-      setRota("catalogo-exclusivas")
-    } else if (tipo == 2) {
       setRota("catalogo")
     } else {
       setRota("acessorios")
     }
   }, [tipo])
-
+  
   const onSubmit = async (data) =>  {
-    // const req = await axios.post(`https://tattooresilia.herokuapp.com/${rota}`)
-    // console.log(data)
-    console.log(tipo)
-    console.log(rota)
+    data.imagem = "https://i.pinimg.com/564x/a8/4b/c5/a84bc5d3ca6f4694ae7d7904fdcfd14c.jpg"
+    const req = await axios.post(`https://tattooresilia.herokuapp.com/${rota}`, data)
     setSucesso(true)
   };
 
   return (
     <div className='divPrincipalCadastro'>
       <div className="cadastroContainer">
-        <h1 className='tituloCadastro'>Cadastro de novo item</h1>
+        <h1 className='tituloCadastro' style={ estilo.resp }>Cadastro de novo item</h1>
 
-        <form action="" className='formCadastro' onSubmit={handleSubmit(onSubmit)}>
+        <form action="" className='formCadastro' style={ estilo.resp } onSubmit={handleSubmit(onSubmit)}>
           <div className="containerInputs">
             <label htmlFor="" className='labelCadastro'>Título</label>
-            <input type="text" className='inputCadastro' { ...register ('TITULO', { required: true, pattern: /([\s\S]+?(?=\b[a-z][)]|$))/ }) } placeholder="Nome"/>
+            <input type="text" className='inputCadastro' { ...register ('titulo', { required: true, pattern: /([\s\S]+?(?=\b[a-z][)]|$))/ }) } placeholder="Nome"/>
 
             <label htmlFor="" className='labelCadastro'>Descrição</label>
-            <textarea type="text" className='inputCadastro inputDesc' { ...register ('DESCRICAO', { required: true, pattern: /([\s\S]+?(?=\b[a-z][)]|$))/ }) } placeholder="Descrição detalhada"/>
+            <textarea type="text" className='inputCadastro inputDesc' { ...register ('descricao', { required: true, pattern: /([\s\S]+?(?=\b[a-z][)]|$))/ }) } placeholder="Descrição detalhada"/>
           </div>
 
           <div className="containerInputs">
             <label htmlFor="" className='labelCadastro'>Tamanho (em cm)</label>
-            <input type="text" className='inputCadastro' { ...register ('TAMANHO', { required: true, pattern: /^[1-9]\d{0,2}/ }) } placeholder="15"/>
+            <input type="text" className='inputCadastro' { ...register ('tamanho', { required: true, pattern: /^[1-9]\d{0,2}/ }) } placeholder="15"/>
 
             <label htmlFor="" className='labelCadastro'>Preço (em R$)</label>
-            <input type="text" className='inputCadastro' { ...register ('PRECO', { required: true, pattern: /^[1-9]\d{0,2}(\.\d{3})*,\d{2}$/ }) } placeholder="199,90"/>
+            <input type="text" className='inputCadastro' { ...register ('preco', { required: true, pattern: /^[1-9]\d{0,2}(\.\d{3})*,\d{2}$/ }) } placeholder="199,90"/>
 
             <label htmlFor="" className='labelCadastro'>Informe o tipo de item</label>
             <select name="" id="" className='inputCadastro' onChange={(e) => setTipo(e.target.value)}>
               <option value=""></option>
-              <option value="1">Tatuagem Exclusiva</option>
-              <option value="2">Tatuagem Simples</option>
-              <option value="3">Acessório</option>
+              <option value="1">Tatuagem</option>
+              <option value="2">Acessório</option>
             </select>
             <button className='btn'>Cadastrar</button>
           </div>
         </form>
+      <div style={{justifyContent: "center", marginTop: "5rem"}}>
+        <h1 className='tituloCadastro' style={estilo.resposta}>Usuário criado com sucesso</h1>
+      </div>
       </div>
     </div>
   )

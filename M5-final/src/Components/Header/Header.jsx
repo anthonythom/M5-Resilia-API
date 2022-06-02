d6591865a9b8c3ec355acfce9ef6eeed25c4668f
Bruno <b.blanquez@yahoo.com.br>
2022-06-02 18:02:09 -0300

    adicionando menu hamburguer e melhorias na responsividade do header e footer

--- M5-final/src/Components/Header/Header.jsx
+++ M5-final/src/Components/Header/Header.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "./header.css"
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const dropDownRef = useRef(null)
+  const [isActive, setIsActive] = useState(false)
+  const onClick = () => setIsActive(!isActive)
+
+  console.log(isActive)
+
   return (
     <section className='divPai'>
       <header className='divTitulo'>
         <h1 className='tituloPrincipal'>Resilia Tattoo Studio</h1>
       </header>
       <section className="menu">
-        <nav className='menuHeader'>
-          <Link to="/" className='menuItens'>Home</Link>
-          <Link to="/catalogo" className='menuItens'>Catálogo</Link>
-          <Link to="/exclusivas" className='menuItens'>Exclusivas</Link>
-          <Link to="/acessorios" className='menuItens'>Acessórios</Link>
-          <Link to="/tatuadores" className='menuItens'>Tatuadores</Link>
-          <Link to="/orcamento" className='menuItens'>Orçamento</Link>
-        </nav>
+        <div className='menuContainer'>
+          <button className='menuMobile' onClick={onClick}>Menu</button>
+          <nav className={`menuHeader ${isActive ? "active" : "inactive"}`} ref={dropDownRef}>
+            <Link to="/" className='menuItens'>Home</Link>
+            <Link to="/catalogo" className='menuItens'>Catálogo</Link>
+            <Link to="/acessorios" className='menuItens'>Acessórios</Link>
+            <Link to="/tatuadores" className='menuItens'>Tatuadores</Link>
+            <Link to="/orcamento" className='menuItens'>Orçamento</Link>
+          </nav>
+        </div>
       </section>
     </section>
   )
--- M5-final/src/Components/NovoItem/NovoItem.jsx
+++ M5-final/src/Components/NovoItem/NovoItem.jsx
@@ -27,8 +27,9 @@ export default function NovoItem() {
   }, [tipo])
   
   const onSubmit = async (data) =>  {
-    data.imagem = "https://i.pinimg.com/564x/a8/4b/c5/a84bc5d3ca6f4694ae7d7904fdcfd14c.jpg"
+    // data.imagem = "https://i.pinimg.com/564x/a8/4b/c5/a84bc5d3ca6f4694ae7d7904fdcfd14c.jpg"
     const req = await axios.post(`https://tattooresilia.herokuapp.com/${rota}`, data)
+    console.log(data)
     setSucesso(true)
   };
 
@@ -38,29 +39,36 @@ export default function NovoItem() {
         <h1 className='tituloCadastro' style={ estilo.resp }>Cadastro de novo item</h1>
 
         <form action="" className='formCadastro' style={ estilo.resp } onSubmit={handleSubmit(onSubmit)}>
-          <div className="containerInputs">
-            <label htmlFor="" className='labelCadastro'>Título</label>
-            <input type="text" className='inputCadastro' { ...register ('titulo', { required: true, pattern: /([\s\S]+?(?=\b[a-z][)]|$))/ }) } placeholder="Nome"/>
+          <div className="containerInputsPai">
+            <div className="containerInputs">
+              <label htmlFor="" className='labelCadastro'>Título</label>
+              <input type="text" className='inputCadastro' { ...register ('titulo', { required: true, pattern: /([\s\S]+?(?=\b[a-z][)]|$))/ }) } placeholder="Nome"/>
 
-            <label htmlFor="" className='labelCadastro'>Descrição</label>
-            <textarea type="text" className='inputCadastro inputDesc' { ...register ('descricao', { required: true, pattern: /([\s\S]+?(?=\b[a-z][)]|$))/ }) } placeholder="Descrição detalhada"/>
+              <label htmlFor="" className='labelCadastro'>Descrição</label>
+              <textarea type="text" className='inputCadastro inputDesc' { ...register ('descricao', { required: true, pattern: /([\s\S]+?(?=\b[a-z][)]|$))/ }) } placeholder="Descrição detalhada"/>
+            </div>
+
+            <div className="containerInputs">
+              <label htmlFor="" className='labelCadastro'>Tamanho (em cm)</label>
+              <input type="text" className='inputCadastro' { ...register ('tamanho', { required: true, pattern: /^[1-9]\d{0,2}/ }) } placeholder="15"/>
+
+              <label htmlFor="" className='labelCadastro'>Preço (em R$)</label>
+              <input type="text" className='inputCadastro' { ...register ('preco', { required: true, pattern: /^[1-9]\d{0,4}(\.\d{3})*,\d{2}$/ }) } placeholder="199,90"/>
+
+              <label htmlFor="" className='labelCadastro'>URL da Imagem</label>
+              <input type="text" className='inputCadastro' { ...register ('imagem', { required: true }) } placeholder="https..."/>
+
+              <label htmlFor="" className='labelCadastro'>Informe o tipo de item</label>
+              <select name="" id="" className='inputCadastro' onChange={(e) => setTipo(e.target.value)}>
+                <option value=""></option>
+                <option value="1">Tatuagem</option>
+                <option value="2">Acessório</option>
+              </select>
+            </div>
           </div>
-
-          <div className="containerInputs">
-            <label htmlFor="" className='labelCadastro'>Tamanho (em cm)</label>
-            <input type="text" className='inputCadastro' { ...register ('tamanho', { required: true, pattern: /^[1-9]\d{0,2}/ }) } placeholder="15"/>
-
-            <label htmlFor="" className='labelCadastro'>Preço (em R$)</label>
-            <input type="text" className='inputCadastro' { ...register ('preco', { required: true, pattern: /^[1-9]\d{0,2}(\.\d{3})*,\d{2}$/ }) } placeholder="199,90"/>
-
-            <label htmlFor="" className='labelCadastro'>Informe o tipo de item</label>
-            <select name="" id="" className='inputCadastro' onChange={(e) => setTipo(e.target.value)}>
-              <option value=""></option>
-              <option value="1">Tatuagem</option>
-              <option value="2">Acessório</option>
-            </select>
-            <button className='btn'>Cadastrar</button>
-          </div>
+              <div className="">
+                <button className='btn'>Cadastrar</button>
+              </div>
         </form>
       <div style={{justifyContent: "center", marginTop: "5rem"}}>
         <h1 className='tituloCadastro' style={estilo.resposta}>Usuário criado com sucesso</h1>

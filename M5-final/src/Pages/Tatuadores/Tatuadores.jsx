df28d4bab7868e5c39b042788be1a1b2a5d040af
Fulano de TalPeruzini <assamitafael@gmail.com>
2022-06-05 17:09:17 -0300

    Adicionar Dados dos Tatuadores

--- M5-final/src/Pages/Tatuadores/Tatuadores.jsx
+++ M5-final/src/Pages/Tatuadores/Tatuadores.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from 'react'
 import "./tatuadores.css"
 
+import {CalendarToday, LocationCity, MailOutline, PermIdentity, PhoneAndroid, Publish} from '@mui/icons-material';
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useForm } from "react-hook-form";
+import axios from "axios"
 
 export default function Tatuadores() {
+  const id = useParams()
+  const urlapi = `https://api-adminpanel.herokuapp.com/users/tatuadores`
+  const [data, setData] = useState({})
+  const { register, handleSubmit } = useForm()
+
+  useEffect(() => {
+    axios.get(urlapi)
+      .then(resposta => {
+        setData(resposta.data)
+        console.log(data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+  const onSubmit = async valor => {
+    console.log(valor)
+    console.log(data)
+    const res = await axios.put(urlapi, valor)
+    console.log(res)
+    };
+
+
 return (  
     <div className='divPrincipal'>
       <div className="divSuperior">

d7eb6066f8a1cbef649cded58ff2e82fde80a675
Fulano de TalPeruzini <assamitafael@gmail.com>
2022-06-06 19:46:02 -0300

    Mudança nos titulos de informações

--- M5-final/src/Pages/Tatuadores/Tatuadores.jsx
+++ M5-final/src/Pages/Tatuadores/Tatuadores.jsx
@@ -19,3 +19,31 @@ console.log(data)
     </div>
   )
 }
+import React from 'react'
+import "./tatuadores.css"
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+
+export default function Catalogo() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://api-adminpanel.herokuapp.com/users/tatuadores`)
+      .then(resposta => {
+        setData(resposta.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  return (  
+    <div className='divPrincipal'>
+      <div className="divSuperior">
+        <h1 className='titulo'>tatuadores</h1>
+      </div>
+  
+      </div>
+  )
+}

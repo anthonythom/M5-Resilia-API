7c6c4c9390b22cd82ccffe307a510027c8390d93
Fulano de TalPeruzini <assamitafael@gmail.com>
2022-06-06 20:36:19 -0300

    Montagem das Cards

--- M5-final/src/Components/Cardt/Cardt.jsx
+++ M5-final/src/Components/Cardt/Cardt.jsx
@@ -7,12 +7,12 @@ export default function Cardt({dados}) {
     return(
 
     <div className='divPrincCard'>
-	    <div className="cardInfos"></div>
+	    <div className="cardInfos">
             <div className="avatar"><img src={dados.avatar} alt="" /></div>
             <div className='nome'>{dados.nome}</div>
 	        <div className='telefone'>{dados.telefone}</div>
             </div>
-
+            </div>
         )
 
     }

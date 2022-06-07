a6d21eb4963c6fc6bc8800caa216c57643deb88b
Yasmin Ramos <yasmincoelhoramos@gmail.com>
2022-06-07 20:18:46 -0300

    novas fotos do carrosseul

--- M5-final/assets/dataCarrossel/slider-data.js
+++ M5-final/assets/dataCarrossel/slider-data.js
@@ -5,17 +5,17 @@ export const sliderData = [
       desc: "This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
     },
     {
-      image: "../assets/home_img/home002.jpg",
+      image: "../assets/home_img/Screenshot_2.png",
       heading: "Slide Dois",
       desc: "This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
     },
     {
-      image: "../assets/home_img/home003.jpg",
+      image: "../assets/home_img/Screenshot_5.png",
       heading: "Slide Três",
       desc: "This is the description of slide three Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
     },
     {
-        image: "../assets/home_img/home004.jpg",
+        image: "../assets/home_img/Screenshot_4.png",
         heading: "Slide Quatro",
         desc: "This is the description of slide four Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
       },
@@ -25,12 +25,12 @@ export const sliderData = [
         desc: "This is the description of slide five Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
       },
       {
-        image: "../assets/home_img/home006.jpg",
+        image: "../assets/home_img/Screenshot_1.png",
         heading: "Slide Seis",
         desc: "This is the description of slide six Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
       },
       {
-        image: "../assets/home_img/home007.jpg",
+        image: "../assets/home_img/Screenshot_4.png",
         heading: "Slide Sete",
         desc: "This is the description of slide seven Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
       }

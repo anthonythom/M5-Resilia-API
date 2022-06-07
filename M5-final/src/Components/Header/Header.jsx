61abbddd6fa0aade9c93a84e3d351191455faf07
Yasmin Ramos <yasmincoelhoramos@gmail.com>
2022-06-07 20:07:06 -0300

    menu dropdown

--- M5-final/src/Components/Header/Header.jsx
+++ M5-final/src/Components/Header/Header.jsx
@@ -7,14 +7,17 @@ export default function Header() {
   const onClick = () => setIsActive(!isActive)
 
   return (
+    <>
     <header className='divPai'>
       <section className='divTitulo'>
         <h1 className='tituloPrincipal'>Resilia Tattoo Studio</h1>
       </section>
-     
-        <section className='menuContainer menu'>
+
+        
+    </header>
+    <section className={`menuContainer menu ${isActive ? "active" : "inactive"}`}>
           <button className={`menuMobile ${isActive ? "active" : "inactive"}`} onClick={onClick}>Menu</button>
-          <nav className={`menuHeader ${isActive ? "active" : "inactive"}`}>
+          <nav className={`menuHeader desktop`}>
             <Link to="/" className='menuItens'>Home</Link>
             <Link to="/catalogo" className='menuItens'>Catálogo</Link>
             <Link to="/acessorios" className='menuItens'>Acessórios</Link>
@@ -22,6 +25,13 @@ export default function Header() {
             <Link to="/orcamento" className='menuItens'>Orçamento</Link>
           </nav>
         </section>
-    </header>
+        <nav className={`menuHeader navbar ${isActive ? "active" : "inactive"}`}>
+            <Link to="/" className='menuItens'>Home</Link>
+            <Link to="/catalogo" className='menuItens'>Catálogo</Link>
+            <Link to="/acessorios" className='menuItens'>Acessórios</Link>
+            <Link to="/tatuadores" className='menuItens'>Tatuadores</Link>
+            <Link to="/orcamento" className='menuItens'>Orçamento</Link>
+          </nav>
+    </>
   )
 }
